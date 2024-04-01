092469db2e40f835efded11fa34d0d956b30aee7
aniruddha j mohile <142429760+mohileAJ@users.noreply.github.com>
2024-03-31 23:08:00 -0400

    intermediate pushing of work to GitHub - 2

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,77 +10,90 @@ date_objects = []
 profit_and_loss = []
 loss = []
 profit = []
+dates2 = []
+profits2 = []
 Daily_changes = []
+cleaned_amts_and_dates = []
 
 # Counters
 total_loss = 0
 total_profit = 0
-Daily_change_count = 0
+
 
 # Set path for the input file, 'budget_data.csv')
 input_path = os.path.join('Resources', 'budget_data.csv')
 output_path = os.path.join("analysis", "result.csv")
 
-# Open the input file
-with open(input_path) as input_file:
+# Open, and read in, the input file; split the str type data on commas
+with open(input_path, 'r') as input_file:
     csv_reader = csv.reader(input_file, delimiter = ',')
-    csv_header = next(input_file)
+    csv_header = next(csv_reader)
 
-# Loop through looking for month in the first element of the input_file
-    for row in csv_reader: 
-        profit_and_loss.append(int(row[1].replace("'", "").replace(" ", "").replace(",", "")))
-        print(type(profit_and_loss[-1]))
-        dates.append(row[0])
-        datetime_str = row[0]
+# Loop through looking for month in the first element of the input_file, and push to an array -- dates[]
+    for date_and_profit_loss in csv_reader: 
+        dates.append(date_and_profit_loss[0])
+        dates_str = date_and_profit_loss[0]
         # parsing the date from its string data type into Python's datetime data type; and adding it into an array called date_objects 
-        date_objects.append(datetime.strptime(datetime_str, '%b-%d'))
-        print(type(date_objects[-1]))
+        date_objects.append(datetime.strptime(dates_str, '%b-%y'))
+        # print(type(date_objects[-1]))
         # splitting the date/month/year, to extract the month from the dataset
-        split_dates = row[0].split("-")
+        split_dates = date_and_profit_loss[0].split("-")
         months.append(split_dates[0])
-    # print(type(row[1]))
-    # print(type(row[0]))
+        profit_and_loss.append(int(date_and_profit_loss[1].replace("'", "").replace(" ", "").replace(",", "")))
+        # print(type(profit_and_loss[-1]))
+        
+    # print(type(date_and_profit_loss[1]))
+    # print(type(date_and_profit_loss[0]))
     # print(profit_and_loss)
 
-with open(input_path) as csv_file:
-    csvreader = csv.reader(csv_file, delimiter = ',')
-    csvheader = next(csv_file)
-    # print(csvreader)
-    for amt_row in csvreader:
-        if "-" in row[1]:
-            loss.append(int(amt_row[1].replace("'", "").replace(",", "")))
-            total_loss += (int(amt_row[1].replace("'", "").replace(",", "")))
+        if "-" in date_and_profit_loss[1]:
+            loss.append(int(date_and_profit_loss[1].replace("'", "").replace(",", "")))
+            total_loss += (int(date_and_profit_loss[1].replace("'", "").replace(",", "")))
         else:
-            profit.append(int(amt_row[1].replace("'", "").replace(",", "")))
-            total_profit += (int(amt_row[1].replace(" ", "").replace(",", "")))
+            profit.append(int(date_and_profit_loss[1].replace("'", "").replace(",", "")))
+            total_profit += (int(date_and_profit_loss[1].replace(" ", "").replace(",", "")))
 
-    Total = total_profit - total_loss
-    print(type(Total))
-    print(type(total_profit))
+    Total = total_profit + total_loss
+    # print(type(Total))
+    # print(type(total_profit))
+    # print(type(total_loss))
+    # print(total_profit)
+    # print(total_loss)    
     # print(Total)
 
-cleaned_amts_and_dates = list(zip(dates, date_objects, profit_and_loss))
+cleaned = list(zip(dates, date_objects, profit_and_loss))
 # print(type([-1]))
 # print(type(row[1]))
 
-cleaned_amts_and_dates.sort(key=lambda x:x[1]) 
-# (date, date_object, profit_and_loss)
-# print(cleaned_amts_and_dates)
+cleaned.sort(key=lambda x:x[1]) 
+# (where x = date, date_object, profit_and_loss)
+# print(type(cleaned))
 
-# Set path for the output file, 'results.csv')
-output_path = os.path.join("analysis", "result.csv")
+# for x in cleaned:
+#     dates2.append(x[0])
+#     profits2.append(x[2])
+# zipped_profits = list(zip(dates2, profits2))
+# # print(zipped_profits)
+# print(type(profits2[1]))
 
-# Open the output file using "write" mode.  Hold the contents in a variable, results_file: , create a header row
-#With open(output_path, 'w', newline='') as results_file:
-    #csvwriter = csv.writer(results_file, delimiter = ',')
-    
-    # Create the first row for a Table header        
-    #csvwriter.writerow('Financial Analysis')
-    #csvwriter.writerow('-------------------')
-    #csvwriter.writerow('Total Months: ' + str(len(months)))
+for i in range((len(cleaned)-1)):
+    # Daily_changes.append(zipped_profits[i+1][0])
+    Daily_changes.append((cleaned[i+1][0],(cleaned[i+1][2]) - (cleaned[i][2])))
+print(Daily_changes)
 
-# For i in range(len(cleaned_amts_and_dates)):
-    # Daily_change_col
+# Calculating average
+averages = []
+for item in Daily_changes:
+    averages.append(item[1])
+print((averages))
+print(len(averages))
+
+
+max_change = max(Daily_changes, key=lambda x:x[1])
+# print(max_change)
+
+min_change = min(Daily_changes, key=lambda x:x[1])
+# print(min_change)
 
 # print(profit_and_loss)
 # print(row[0])
@@ -89,7 +102,25 @@ output_path = os.path.join("analysis", "result.csv")
         
 
     
-print("Financial Analysis")
-print("--------------------")
+print("Financial Analysis\n--------------------")
 print("Total Months: " + str(len(months)))
 print("Total: " + str(Total))
+print(f"Average Change: ${sum(averages)/len(averages):.2f}")
+print(f"Greatest Increase in Profits: {max_change[0]} (${max_change[1]})")
+print(f"Greatest Decrease in Profits: {min_change[0]} (${min_change[1]})")
+
+
+# Set path for the output file, 'results.csv')
+output_path = os.path.join("analysis", "result.txt")
+
+# Open the output file using "write" mode.  Hold the contents in a variable, results_file: , create a header row
+with open(output_path, 'w') as results_file:
+  
+    # Create the first two row for a Table header        
+    print('Financial Analysis', file=results_file)
+    print('-------------------', file=results_file)
+    print('Total Months: ' + str(len(months)), file=results_file)
+    print("Total: " + str(Total), file=results_file)
+    print(f"Average Change: ${sum(averages)/len(averages):.2f}", file=results_file)
+    print(f"Greatest Increase in Profits: {max_change[0]} (${max_change[1]})", file=results_file)
+    print(f"Greatest Decrease in Profits: {min_change[0]} (${min_change[1]})", file=results_file)
